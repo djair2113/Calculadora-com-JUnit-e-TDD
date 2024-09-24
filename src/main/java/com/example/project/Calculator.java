f1a0290a64b16db58daebd3dce9e9f782344e2b0
Cizok <tarcisioneves98@gmail.com>
2024-09-23 22:02:52 -0300

    Teste_Calculadora

---
+++ src/main/java/com/example/project/Calculator.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015-2024 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package com.example.project;
+
+public class Calculator {
+
+	public int soma(int a, int b) {
+		return a + b;
+	}
+
+	public int sub(int a, int b) {
+		return a - b;
+	}
+
+	public int divi(int a, int b) {
+		return a / b;
+	}
+	
+	public int muti(int a, int b) {
+		return a * b;
+	}
+	
+
+}
